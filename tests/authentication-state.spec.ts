dd51225b2476412a81d303aca32b39ac3dbf222c
qahive-art <atthaboon.s@qahive.com>
2022-08-06 22:17:15 +0700

    Add demo script

--- tests/authentication-state.spec.ts
+++ tests/authentication-state.spec.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
 test.only('Reuse Authentication state', async ({ page }) => {
-  
+  await page.goto('https://demoqa.com/profile');
+  await expect(page.locator('id=userName-value')).toContainText('demoqahive01');
 });
