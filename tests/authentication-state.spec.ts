908edca2515f98ab753ec14671893e5eba1255df
qahive-art <atthaboon.s@qahive.com>
2022-08-06 22:19:41 +0700

    Remove only

--- tests/authentication-state.spec.ts
+++ tests/authentication-state.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
-test.only('Reuse Authentication state', async ({ page }) => {
+test('Reuse Authentication state', async ({ page }) => {
   await page.goto('https://demoqa.com/profile');
   await expect(page.locator('id=userName-value')).toContainText('demoqahive01');
 });
